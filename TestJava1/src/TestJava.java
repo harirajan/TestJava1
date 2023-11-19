ad4fa7b49dd16aa0ddcfc6e99f5dacb8563556ad
harirajan <harirajan.a@gmail.com>
2023-11-19 09:42:35 +0530

    test

--- TestJava1/src/TestJava.java
+++ TestJava1/src/TestJava.java
@@ -3,7 +3,7 @@ public class TestJava {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello2");
-		System.out.println("Next message");
+		System.out.println("Next message1");
 	}
 
 }
