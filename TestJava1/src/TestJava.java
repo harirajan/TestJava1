4e8a247e5d6fa8d7fd0e5b559c38150453a6c872
harirajan <harirajan.a@gmail.com>
2023-11-20 12:16:10 +0530

    break build

--- TestJava1/src/TestJava.java
+++ TestJava1/src/TestJava.java
@@ -3,7 +3,7 @@ public class TestJava {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello2");
-		System.out.println("Next message1")s
+		System.out.println("Next message1");
 	}
 
 }
